c8e190882c947a65c61b7fed086f62e4053319b5
Santi <santiago.odz12@gmail.com>
2024-09-20 12:08:53 +0200

    Added 2.3

---
+++ 2.3/2.3/PriorityQueue.h
@@ -0,0 +1,12 @@
+//
+//  PriorityQueue.h
+//  2.3
+//
+//  Created by Santi Ochoa on 9/20/24.
+//
+
+#ifndef PriorityQueue_h
+#define PriorityQueue_h
+
+
+#endif /* PriorityQueue_h */
---
+++ 2.3/2.3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2.3
+//
+//  Created by Santi Ochoa on 9/20/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
