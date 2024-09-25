977b1b0a54f3623310b154f788611a115064fbe8
Alexboni97 <alexboni@ucm.es>
2024-09-25 10:51:14 +0200

    correct

--- 2.5/urgencias.cpp
+++ 2.5/urgencias.cpp
@@ -8,10 +8,10 @@
 
 #include <iostream>
 #include <fstream>
-#include <...>
+#include <queue>
 using namespace std;
 
-#include "..."  // propios o los de las estructuras de datos de clase
+//#include "..."  // propios o los de las estructuras de datos de clase
 
 /*@ <answer>
 
@@ -27,13 +27,51 @@ using namespace std;
 // ================================================================
 //@ <answer>
 
+struct Paciente {
+    int orden;
+    string nombre;
+    int gravedad;
+
+    Paciente(int ordenEntra, string nombreEntra, int gravedadEntra): orden(ordenEntra), nombre(nombreEntra), gravedad(gravedadEntra) { }
+};
+
+bool operator<(Paciente const &p1, Paciente const &p2) {
+    return p1.gravedad < p2.gravedad || (p1.gravedad == p2.gravedad && p1.orden > p2.orden);
+}
+
+void tratarPaciente(priority_queue<Paciente> &cola) {
+    Paciente p = cola.top();
+    cola.pop();
+    cout << p.nombre << "\n";
+}
+
 bool resuelveCaso() {
 
    // leer los datos de la entrada
+   int num;
+   cin >> num;
 
-   if (caso especial)
-      return false;
+   if (!num) return false;
 
+   priority_queue<Paciente> cola;
+
+   for (int i = 0; i < num; i++) {
+       char caso;
+       cin >> caso;
+
+       string nombre;
+       int gravedad;
+
+       if (caso == 'I') {
+           cin >> nombre >> gravedad;
+           cola.emplace(i, nombre, gravedad);
+       }
+       else {
+        tratarPaciente(cola);
+       }
+   }
+
+   cout << "---\n";
    // resolver el caso posiblemente llamando a otras funciones
 
    // escribir la solución
