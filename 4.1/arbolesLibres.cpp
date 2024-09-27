0df1073ae963218c5f283210ed7f1f98ae462bf1
Santi <santiago.odz12@gmail.com>
2024-09-27 14:26:40 +0200

    Correct

---
+++ 4.1/arbolesLibres.cpp
@@ -0,0 +1,92 @@
+
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXX) de los autores de la solución.
+ * TAIS009 Alex Guillermo Bonilla Taco
+ * TAIS069 Santiago Ochoa de Zabalegui Velasco
+ *@ </authors> */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+#include "Grafo.h" // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+ 
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+ 
+ @ </answer> */
+
+
+// ================================================================
+// Escribe el código completo de tu solución aquí debajo
+// ================================================================
+//@ <answer>
+
+void recorreGrafo(const Grafo& g, int verticeAct, vector<bool>& verticesVisitados, int& nVisitados) {
+  verticesVisitados[verticeAct] = true;
+  for (int i : g.ady(verticeAct)) {
+    if (!verticesVisitados[i]) {
+      recorreGrafo(g, i, verticesVisitados, nVisitados);
+      nVisitados++;
+    }
+  }
+}
+
+bool resolver(const Grafo &g) {
+  vector<bool> verticesVisitados(g.V(), false);
+  int nVisitados = 1;
+  recorreGrafo(g, 0, verticesVisitados, nVisitados);
+  return nVisitados == g.V() && (g.A() == g.V() - 1); // he visitado todos los vertices y el numero de aristas es el numero de vertices - 1
+}
+
+// resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+  
+  // leer los datos de la entrada
+  int vertices, aristas;
+  
+  cin >> vertices;
+  
+  if (!cin) return false;
+  
+  cin >> aristas;
+  
+  Grafo g(vertices);
+  
+  int p1, p2;
+  
+  for (int i = 0; i < aristas; i++) {
+    cin >> p1 >> p2;
+    g.ponArista(p1, p2);
+  }
+
+  cout << (resolver(g) ? "SI" : "NO") << "\n";
+  return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+  // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+  std::ifstream in("casos.txt");
+  auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+  
+  while (resuelveCaso());
+  
+  // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+  std::cin.rdbuf(cinbuf);
+  system("PAUSE");
+#endif
+  return 0;
+}
