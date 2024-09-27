3fed583d1a9b60136bfb7b63d2756973edf51aa6
Alexboni97 <alexboni@ucm.es>
2024-09-27 12:31:31 +0200

    correct

--- 3.1/aves.cpp
+++ 3.1/aves.cpp
@@ -2,8 +2,8 @@
 /*@ <authors>
  *
  * Nombre, apellidos y usuario del juez (TAISXX) de los autores de la solución.
- * TAIS009 Alex Guillermo Bonilla Taco 
- * TAIS069 Santiago Ochoa de Zabalegui Velasco 
+ * TAIS009 Alex Guillermo Bonilla Taco
+ * TAIS069 Santiago Ochoa de Zabalegui Velasco
  *@ </authors> */
 
 #include <iostream>
@@ -36,8 +36,8 @@ bool resuelveCaso() {
    if (edad == 0 && parejas == 0)
       return false;
 
-   priority_queue<int, greater<int>> mini;
-   priority_queue<int> maxi;
+   priority_queue<long long int> mini;
+   priority_queue<long long int,vector<long long int>, greater<long long int>> maxi;
    
    maxi.push(edad);
 
@@ -45,9 +45,36 @@ bool resuelveCaso() {
       long long int p1, p2, medio;
       cin >> p1 >> p2;
       medio = maxi.top();
+      if(p1 < medio)
+         mini.push(p1);
+      else maxi.push(p1);
       
+      if(p2 < medio)
+         mini.push(p2);
+      else maxi.push(p2);
+     
+     long long int tamMaxi, tamMini;
+     tamMaxi = maxi.size();
+     tamMini = mini.size();
+      
+     while(abs(tamMaxi - tamMini) > 1) {
+         long long int topDeLaColaQueToque;
+         if (maxi.size() > mini.size()) {
+            topDeLaColaQueToque = maxi.top();
+            maxi.pop();
+            mini.push(topDeLaColaQueToque);
+         } else {
+            topDeLaColaQueToque = mini.top();
+            mini.pop();
+            maxi.push(topDeLaColaQueToque);
+         }
+         tamMaxi = maxi.size();
+         tamMini = mini.size();
+      }
+      long long int mostrar = (maxi.size() > mini.size()) ? maxi.top(): mini.top();
+      cout << mostrar << " ";
    }
-   
+   cout << "\n";
    // resolver el caso posiblemente llamando a otras funciones
 
    // escribir la solución
@@ -61,16 +88,16 @@ bool resuelveCaso() {
 int main() {
    // ajustes para que cin extraiga directamente de un fichero
 #ifndef DOMJUDGE
-   std::ifstream in("casos.txt");
-   auto cinbuf = std::cin.rdbuf(in.rdbuf());
+  std::ifstream in("casos.txt");
+  auto cinbuf = std::cin.rdbuf(in.rdbuf());
 #endif
 
    while (resuelveCaso());
 
    // para dejar todo como estaba al principio
 #ifndef DOMJUDGE
-   std::cin.rdbuf(cinbuf);
-   system("PAUSE");
+  std::cin.rdbuf(cinbuf);
+  system("PAUSE");
 #endif
    return 0;
 }
