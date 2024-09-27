611beab87d24fba44fdaf504c9204147c979374c
Alexboni97 <alexboni@ucm.es>
2024-09-27 22:23:42 +0200

    correct

--- 3.1/aves.cpp
+++ 3.1/aves.cpp
@@ -18,6 +18,13 @@ using namespace std;
  se resuelve el problema y cuál es el coste de la solución, en función
  del tamaño del problema.
 
+ El problema se resuelve con dos colas de prioridad, una de maximos que almacenaran las edades menores a la mediana y 
+ uno de minimos que almacenara las edades mayores de la mediana. La mediana sera el resultado que se mostrara por pantalla, 
+ y siempre se encontrara en la cola con tamaño mayor y siempre las dos colas tendran como maximo una diferencia de tamaño de 1,
+ si en algun momento en cada pareja de entrada se tiene mas nodos de diferencia, se traspasara de sus tops de una cola o otra, 
+ tantas veces como fuese necesario hasta que vuelvan a tener la diferencia maxima de 1.
+
+ El coste de resuelveCaso() es O(K log n), siendo K el numero total de parejas de aves y log n, el coste de la funcion push()
  @ </answer> */
 
 
@@ -29,55 +36,54 @@ using namespace std;
 bool resuelveCaso() {
 
    // leer los datos de la entrada
-   long long int edad, parejas;
+   int edad, parejas;
 
    cin >> edad >> parejas;
 
    if (edad == 0 && parejas == 0)
       return false;
 
-   priority_queue<long long int> mini;
-   priority_queue<long long int,vector<long long int>, greater<long long int>> maxi;
+   priority_queue<int> mini;
+   priority_queue<int,vector<int>, greater<int>> maxi;
    
    maxi.push(edad);
 
-   for (long long int i = 0; i < parejas; i++) {
-      long long int p1, p2, medio;
+   for (int i = 0; i < parejas; i++) { //O(k log n)
+      int p1, p2, medio;
       cin >> p1 >> p2;
-      medio = maxi.top();
+      medio = maxi.top(); //O(1)
       if(p1 < medio)
-         mini.push(p1);
-      else maxi.push(p1);
+         mini.push(p1); //O(log n)
+      else maxi.push(p1); //O(log n)
       
       if(p2 < medio)
          mini.push(p2);
       else maxi.push(p2);
      
-     long long int tamMaxi, tamMini;
-     tamMaxi = maxi.size();
-     tamMini = mini.size();
+     int tamMaxi, tamMini;
+     tamMaxi = maxi.size(); //O(1)
+     tamMini = mini.size(); //O(1)
       
-     while(abs(tamMaxi - tamMini) > 1) {
-         long long int topDeLaColaQueToque;
+     while(abs(tamMaxi - tamMini) > 1) {  //O(1*log n)
+         int topDeLaColaQueToque;
          if (maxi.size() > mini.size()) {
             topDeLaColaQueToque = maxi.top();
-            maxi.pop();
-            mini.push(topDeLaColaQueToque);
+            maxi.pop(); //O(1)
+            mini.push(topDeLaColaQueToque);  //O(log n)
          } else {
             topDeLaColaQueToque = mini.top();
-            mini.pop();
+            mini.pop(); //O(1)
             maxi.push(topDeLaColaQueToque);
          }
          tamMaxi = maxi.size();
          tamMini = mini.size();
       }
-      long long int mostrar = (maxi.size() > mini.size()) ? maxi.top(): mini.top();
+      // escribir la solución
+      int mostrar = (maxi.size() > mini.size()) ? maxi.top(): mini.top();
       cout << mostrar << " ";
    }
    cout << "\n";
-   // resolver el caso posiblemente llamando a otras funciones
 
-   // escribir la solución
 
    return true;
 }
--- 3.2/drones.cpp
+++ 3.2/drones.cpp
@@ -12,9 +12,26 @@
 #include <unordered_set>
 
 using namespace std;
+/*@ <answer>
 
-// resuelve un caso de prueba, leyendo de la entrada la
-// configuración, y escribiendo la respuesta
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ El problema se resuelve con dos colas de prioridad, de maximos que almacenara la carga disponible de cada tipo de pila, 
+ tambien se usa dos unordered_multiset para almacenar la carga de las pilas al terminar cada sabado, por su coste O(1) al insertar,
+ para luego recorrerla y pushear de nuevo a la cola de prioridad correspondiente. Y volver a realizar tantas iteraciones hasta que una 
+ de las colas este vacia, y acumulando en la variable horasVuelo, el numero de horas total que se podrán poner a volar los drones.
+ Por ultimo, en cada caso mostramos por pantalla las horas de vuelo en cada iteracion.
+
+ El coste de resuelveCaso() es O(K N log n), siendo K el numero maximo de pilas entre los dos tipos, N el tamaño del unordered_multiset
+ y log n, el coste de la funcion push() de priority_queue
+ @ </answer> */
+
+// ================================================================
+// Escribe el código completo de tu solución aquí debajo
+// ================================================================
+//@ <answer>
 bool resuelveCaso() {
   
   // leer los datos de la entrada
@@ -31,27 +48,27 @@ bool resuelveCaso() {
   
   cin >> nPilas9V >> nPilas1V;
   
-  for (int i = 0; i < nPilas9V; i++) {
+  for (int i = 0; i < nPilas9V; i++) { //O(N log n)
     cin >> horasVuelo;
-    pilas9V.push(horasVuelo);
+    pilas9V.push(horasVuelo); //O(log n)
   }
   
-  for (int i = 0; i < nPilas1V; i++) {
+  for (int i = 0; i < nPilas1V; i++) { //O(N log n)
     cin >> horasVuelo;
-    pilas1V.push(horasVuelo);
+    pilas1V.push(horasVuelo); //O(log n)
   }
   
-  if (pilas1V.size() == 0 || pilas9V.size() == 0) return true;
+  if (pilas1V.size() == 0 || pilas9V.size() == 0) return true; //no hace falta por la precondicion
   
-  while (pilas1V.size() > 0 && pilas9V.size() > 0) {
+  while (pilas1V.size() > 0 && pilas9V.size() > 0) { //O(K * N *log n)
     
     int horasTotales = 0;
     
-    for(int i = 0; i < nDrones; i++) {
+    for(int i = 0; i < nDrones; i++) { //O(N)
       if (pilas1V.size() > 0 && pilas9V.size() > 0){
         
-        int pila1 = pilas1V.top();
-        int pila9 = pilas9V.top();
+        int pila1 = pilas1V.top(); //O(1)
+        int pila9 = pilas9V.top(); //O(1)
         
         pilas9V.pop();
         pilas1V.pop();
@@ -60,20 +77,20 @@ bool resuelveCaso() {
           horasTotales += pila1;
         } else if (pila1 > pila9) {
           horasTotales += pila9;
-          almacen1V.insert((pila1 - pila9));
+          almacen1V.insert((pila1 - pila9)); //O(1)
         } else {
           horasTotales += pila1;
-          almacen9V.insert((pila9 - pila1));
+          almacen9V.insert((pila9 - pila1)); //O(1)
         }
       }
     }
     
-    for(auto it = almacen1V.begin(); it != almacen1V.end(); it++) {
-      pilas1V.push(*it);
+    for(auto it = almacen1V.begin(); it != almacen1V.end(); it++) {  //O(N log n)
+      pilas1V.push(*it); //O(log n)
     }
     
-    for(auto it = almacen9V.begin(); it != almacen9V.end(); it++) {
-      pilas9V.push(*it);
+    for(auto it = almacen9V.begin(); it != almacen9V.end(); it++) { //O(N log n)
+      pilas9V.push(*it); //O(log n)
     }
     
     almacen1V.clear();
