4c2f5781f9818d80e1f06806ec93a205ecd1d118
Alexboni97 <alexboni@ucm.es>
2024-10-02 10:30:18 +0200

    correct

--- 3.L/audiencias.cpp
+++ 3.L/audiencias.cpp
@@ -8,10 +8,11 @@
 
 #include <iostream>
 #include <fstream>
-#include <...>
+#include <queue>
+#include <vector>
 using namespace std;
 
-#include "..."  // propios o los de las estructuras de datos de clase
+#include "IndexPQ.h" // propios o los de las estructuras de datos de clase
 
 /*@ <answer>
 
@@ -27,17 +28,67 @@ using namespace std;
 // ================================================================
 //@ <answer>
 
+struct canal {
+   int minutos;
+   int numero;
+};
+
+bool operator<(const canal &c1, const canal &c2) {
+   return c1.minutos < c2.minutos || (c1.minutos == c2.minutos && c1.numero > c2.numero);
+}
+
 bool resuelveCaso() {
+   int d,c,n;
 
    // leer los datos de la entrada
-
+   cin >> d;
    if (!std::cin)  // fin de la entrada
       return false;
-
+   cin >> c >> n;
    // resolver el caso posiblemente llamando a otras funciones
+   IndexPQ<int, greater<int>> canales(c);
+   vector<canal> minutos;
+   int p;
+   
+   for (int i = 0; i < c; i++) {
+      cin >> p;
+      canales.push(i, p);
+      minutos.push_back({0,i + 1});
+   }
 
    // escribir la solución
+   int anterior = 0, actual;
+   for(int i = 0; i < n; i++) {
+     
+       cin >> actual;
 
+      int num, audiencia;
+
+      cin >> num;
+
+      minutos[canales.top().elem].minutos += actual - anterior;
+
+      while (num != -1) {
+         cin >> audiencia;
+         canales.update(num - 1, audiencia);
+         cin >> num;
+      }
+      anterior = actual;
+   }
+
+   minutos[canales.top().elem].minutos += d - anterior;
+   
+   priority_queue<canal> salida;
+   for (int i = 0; i < c; i++) {
+      salida.push(minutos[i]);
+   }
+   
+   for (int i = 0; i < c; i++) {
+      canal c = salida.top();
+      salida.pop();
+      if (c.minutos != 0) cout << c.numero << " " << c.minutos << "\n";
+   }
+   cout<<"---\n";
    return true;
 }
 
