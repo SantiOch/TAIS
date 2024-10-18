03ff6678fa6e59f3ae5c2e78f20dfd7c72f0ad00
Santi <santiago.odz12@gmail.com>
2024-10-18 12:31:10 +0200

    Correct

--- 6.3/bicicletas.cpp
+++ 6.3/bicicletas.cpp
@@ -2,7 +2,7 @@
 /*@ <authors>
  *
  * Nombre, apellidos y usuario del juez (TAISXX) de los autores de la solución.
- * TAIS009 Alex Guillermo Bonilla Taco 
+ * TAIS009 Alex Guillermo Bonilla Taco
  * TAIS069 Santiago Ochoa de Zabalegui Velasco
  *@ </authors> */
 
@@ -38,28 +38,26 @@ private:
    bool existe_arm;
 
 public:
-   Valor costeARM() const
-   {
+   Valor costeARM() const {
       return coste;
    }
-   bool existeARM()const{
+   bool existeARM()const {
       return existe_arm;
    }
-   std::vector<Arista<Valor>> const &ARM() const
-   {
+  
+   std::vector<Arista<Valor>> const &ARM() const {
       return _ARM;
    }
    ARM_Kruskal(GrafoValorado<Valor> const &g) : coste(0),existe_arm(false)
    {
-      PriorityQueue<Arista<Valor>> pq(g.aristas());
+     if(g.V() == 1) existe_arm = true;
+       PriorityQueue<Arista<Valor>> pq(g.aristas());
       ConjuntosDisjuntos cjtos(g.V());
-      while (!pq.empty())
-      {
+      while (!pq.empty()) {
          auto a = pq.top();
          pq.pop();
          int v = a.uno(), w = a.otro(v);
-         if (!cjtos.unidos(v, w))
-         {
+         if (!cjtos.unidos(v, w)) {
             cjtos.unir(v, w);
             _ARM.push_back(a);
             coste += a.valor();
@@ -82,17 +80,15 @@ bool resuelveCaso() {
    GrafoValorado<int>g(n);
    // resolver el caso posiblemente llamando a otras funciones
    int v,w,coste;
-   for (int i = 0; i < p; i++)
-   {
-      cin>>v>>w>>coste;
+   for (int i = 0; i < p; i++) {
+      cin >> v >> w >> coste;
       g.ponArista({v - 1, w - 1, coste});
    }
    ARM_Kruskal arm_k(g);
-   bool sol=arm_k.existeARM();
+   bool sol = arm_k.existeARM();
    // escribir la solución
-   if (sol)
-   {
-      cout<<arm_k.costeARM()<<"\n";
+   if (sol) {
+      cout << arm_k.costeARM() << "\n";
    }else
       cout<<"No hay puentes suficientes\n";
    
