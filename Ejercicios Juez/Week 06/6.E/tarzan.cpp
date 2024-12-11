a9e75fb4abbea36b38023f231423dcdd47f69a62
Alexboni97 <alexboni@ucm.es>
2024-12-11 09:29:38 +0100

    coomentarios ev

--- Ejercicios Juez/Week 06/6.E/tarzan.cpp
+++ Ejercicios Juez/Week 06/6.E/tarzan.cpp
@@ -18,10 +18,20 @@ using namespace std;
 
 /*@ <answer>
  
- Escribe aquí un comentario general sobre la solución, explicando cómo
- se resuelve el problema y cuál es el coste de la solución, en función
- del tamaño del problema.
- 
+Para resolver el problema creamos un grafo valorado con los datos de entrada, siendo los nodos los arboles
+y las aristas son las cuerdas que unen los arboles y el valor es la longitud de la cuerda. El problema lo
+resolvemos con el algoritmo de kruskal para conseguir un arbol de recubrimiento minimo. En la variable cuerda,
+llevamos la longitud de la arista mas grande que esta en el arm. En la variable llegaFinal ponemos a true si
+recorre el arbol completo o el tamaño del arbol es 1, false en caso contrario. Si es true llegaFinal mostramos por
+pantalla el contenido de la variable cuerda, en otro caso mostramos Imposible
+
+El coste de resuelveCaso() es O(M + M log M), siendo el coste relacionado a O(M), el coste de
+costruir el grafo con M aristas,y el coste de O(M log M), es el de hacer el kruskal del grafo previamente construido. 
+Las demas operaciones es de coste costante O(1).
+
+El coste en espacio es de O(N + M) ya que tenemos un grafo valorado de tamaño N y el algoritmo de
+Kruskal tiene coste de O(M) en espacio adicional;
+
  @ </answer> */
 
 
